e4fd24c4c99e47f3222e094871c1abb320a97ae9
madhvigupta <meetmadhvi@gmail.com>
2019-01-12 02:41:07 +0530

    Removed unused files
    reverse geocode handling
    
    syncing problem solved

---
+++ locations/src/main/java/com/softminesol/locations/locationmanager/data/Geometry.java
@@ -0,0 +1,41 @@
+package com.softminesol.locations.locationmanager.data;
+
+public class Geometry{
+	private Viewport viewport;
+	private Location location;
+	private String locationType;
+
+	public void setViewport(Viewport viewport){
+		this.viewport = viewport;
+	}
+
+	public Viewport getViewport(){
+		return viewport;
+	}
+
+	public void setLocation(Location location){
+		this.location = location;
+	}
+
+	public Location getLocation(){
+		return location;
+	}
+
+	public void setLocationType(String locationType){
+		this.locationType = locationType;
+	}
+
+	public String getLocationType(){
+		return locationType;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Geometry{" + 
+			"viewport = '" + viewport + '\'' + 
+			",location = '" + location + '\'' + 
+			",location_type = '" + locationType + '\'' + 
+			"}";
+		}
+}
